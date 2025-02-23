c97a441241bda166afa25e0fa4735fa593c07c0a
SaifySheikh <sheikhmm_4@rknec.edu>
2025-02-23 09:53:50 +0530

    Solved

---
+++ 889. Construct Binary Tree from Preorder and Postorder Traversal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+        TreeNode* construct(int preStart, int preEnd, int postStart, vector<int> &preorder, vector<int> &postorder){
+            if(preStart > preEnd)
+                return nullptr;
+            if(preStart == preEnd)
+                return new TreeNode(preorder[preStart]);
+            TreeNode *root = new TreeNode(preorder[preStart]);
+            int left = preorder[preStart+1], noOfElementsOnLeft = 1;
+            while(postorder[postStart + noOfElementsOnLeft - 1]!=left)
+                noOfElementsOnLeft++;
+            root->left = construct(preStart+1, preStart + noOfElementsOnLeft, postStart, preorder, postorder);
+            root->right = construct(preStart+1+noOfElementsOnLeft, preEnd, postStart+noOfElementsOnLeft, preorder, postorder);
+            return root;
+        }
+    
+        TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+            int n = preorder.size();
+            return construct(0, n-1, 0, preorder, postorder);
+        }
+    };
